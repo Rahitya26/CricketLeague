42725a626e1cf2c7eb79b039ba9f00503c327b05
Rahitya26 <karamchedurahitya@gmail.com>
2025-04-28 23:17:12 +0530

    view engine fix

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const PORT = 3000;
 // --- Middleware ---
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(session({
